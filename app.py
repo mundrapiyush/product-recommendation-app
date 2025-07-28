c5ed98f0c8abc46c36eae46d0faec1199726800b
PIYUSH MUNDRA <PIYUSH.Mundra@ibm.com>
2025-07-28 12:49:15 +0530

    feat: add support for input and output

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, Heroku!"
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    username = request.form.get('username', '')
+
+    # Example logic to generate table data based on username
+    # Here, we create a list of dictionaries representing rows in the table
+    table_data = [
+        {"ID": i+1, "Username": username, "Info": f"Info {i+1}"} for i in range(5)
+    ]
+
+    return render_template('results.html', username=username, table_data=table_data)
 
 if __name__ == '__main__':
     app.run()
